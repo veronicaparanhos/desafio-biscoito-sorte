d764921aaa0a210981063da30e2ac5a2bea42531
Veronica Paranhos <veronicahp@gmail.com>
2023-09-21 11:30:44 -0300

    html e css concluídos

---
+++ main.js
@@ -0,0 +1,16 @@
+const frases = [
+  "Se alguém está tão cansado que não possa te dar um sorriso, deixa-lhe o teu.",
+  "A vida trará coisas boas se tiver paciência.",
+  "Não compense na ira o que lhe falta na razão.",
+  "Defeitos e virtudes são apenas dois lados da mesma moeda",
+  "A juventude não é uma época da vida, é um estado de espírito.",
+  "Siga os bons e aprenda com eles.",
+  "A adversidade é um espelho que reflete o verdadeiro eu.",
+  "Sem o fogo do entusiasmo, não há o calor da vitória",
+  "Espere pelo mais sábio dos conselhos: o tempo",
+];
+
+const toggleSreen = () => {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+};
