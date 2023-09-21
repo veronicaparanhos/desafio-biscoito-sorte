008c22abe93d5afa6da3b237a1d33034de912240
Veronica Paranhos <veronicahp@gmail.com>
2023-09-21 13:21:36 -0300

    desafio completado

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
-const frases = [
-  "Se alguém está tão cansado que não possa te dar um sorriso, deixa-lhe o teu.",
-  "A vida trará coisas boas se tiver paciência.",
+const show = document.querySelector("#show");
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+const botao = document.querySelector("button");
+
+const phrases = [
+  "Espere pelo mais sábio dos conselhos: o tempo.",
+  "Todas as coisas são difíceis antes de se tornarem fáceis.",
+  "Espere pelo mais sábio dos conselhos: o tempo.",
+  "Vencer é 90 por cento suor e 10 por cento de engenho.",
+  "O amor está sempre mais próximo do que você imagina.",
+  "Você é do tamanho do seu sonho.",
   "Não compense na ira o que lhe falta na razão.",
-  "Defeitos e virtudes são apenas dois lados da mesma moeda",
-  "A juventude não é uma época da vida, é um estado de espírito.",
-  "Siga os bons e aprenda com eles.",
-  "A adversidade é um espelho que reflete o verdadeiro eu.",
-  "Sem o fogo do entusiasmo, não há o calor da vitória",
-  "Espere pelo mais sábio dos conselhos: o tempo",
+  "Realize o óbvio, pense no improvável e conquiste o impossível.",
+  "O nosso primeiro e último amor é… o amor-próprio.",
+  "Deixe de lado as preocupações e seja feliz.",
+  "A vontade das pessoas é a melhor das leis.",
+  "Nós somos o que pensamos.",
+  "A maior barreira para o sucesso é o medo do fracasso.",
 ];
 
 const toggleSreen = () => {
   screen1.classList.toggle("hide");
   screen2.classList.toggle("hide");
 };
+
+const replacePhrases = () => {
+  let selectedPhrases = Math.floor(Math.random() * 12 + 1);
+  return selectedPhrases;
+};
+
+const reload = () => {
+  document.location.reload(true);
+};
+
+document.getElementById("replace").innerHTML = phrases[replacePhrases()];
+
+show.addEventListener("click", toggleSreen);
+botao.addEventListener("click", reload);
